f6d12587cecefc6c2f395a15216ccd1181aeaba9
maica-angela <accia.alena21@gmail.com>
2024-10-03 22:05:07 +0800

    add support

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
+import { clickCookies } from '../cypress/support/cookies.js';
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://web-dev3-k8s.webopsvolaris.com/')
+    cy.wait(5000)
+    cy.get('[class="radius-6"]').should('be.visible').click().wait(3000)
+    cy.get('[name="fnameDestination"]').should('be.visible').wait(3000).type('CUN')
+    cy.contains('CUN').click()
+
+    clickCookies();
+
+    //add function/checker: is the cookie pop up there? if yes, click Accept all
   })
 })
---
+++ cypress/support/cookies.js
@@ -0,0 +1,6 @@
+
+export function clickCookies(){
+    cy.get('[id="onetrust-banner-sdk"]').contains('[id="onetrust-accept-btn-handler"]').click()
+}
+       
+   
