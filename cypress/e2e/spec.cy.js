eadde3e1728b61ed99c3706bec39accbfb62be2e
maica-angela <accia.alena21@gmail.com>
2024-10-04 00:27:09 +0800

    click cookies and calendar

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,20 @@
-import { clickCookies } from '../cypress/support/cookies.js';
+import { clickCookies } from '/cypress/e2e/cookies.cy.js';
 
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://web-dev3-k8s.webopsvolaris.com/')
     cy.wait(5000)
     cy.get('[class="radius-6"]').should('be.visible').click().wait(3000)
-    cy.get('[name="fnameDestination"]').should('be.visible').wait(3000).type('CUN')
+    cy.get('[name="fnameDestination"]').should('be.visible').type('CUN')
     cy.contains('CUN').click()
-
     clickCookies();
-
     //add function/checker: is the cookie pop up there? if yes, click Accept all
+
+    cy.wait(5000)
+    cy.get('[class="dialog-top-text col-12 col-md-6 ng-star-inserted"]')
+      .get('[class="btn-calendar d-none d-md-block mat-flat-button mat-button-base mat-secondary"]').click()
+
+    cy.get('[class="mat-form-field-infix"]').click()
+    
   })
 })
